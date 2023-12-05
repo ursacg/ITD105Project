0a350deeb96e3a802ec8ae5dc523260d2b221106
ursacg <ursabiacg@gmail.com>
2023-12-06 01:42:50 +0800

    updated python file and csv file

--- BigDataProject.py
+++ BigDataProject.py
@@ -3,16 +3,34 @@ import plotly.express as px
 import pandas as pd
 
 app = Dash(__name__)
+df = pd.read_csv('suicide-rate.csv')
 
 app.layout = html.Div([
-    html.H1("Suicide Rates of Countries from 1987 to 2014", style={'textAlign': 'center'}),
-    html.Div(children=[
-        html.Label("Insert CSV file URL here: "),
-        dcc.Input(id='file-input', value='csv url file', type='url', style={'margin-left': '5px'})
-        ], style={'justifyContent': 'center', 'display': 'flex'}
-    ),
-    html.P("choropleth map here", style={'textAlign': 'center'})
+    html.Div([
+        html.H1("Suicide Rates by Year", style={'textAlign': 'center'})
+    ]),
+    html.Div([
+        dcc.Graph(id='csv-graph'),
+    ]),
+    html.Div([
+        dcc.Input(id='csv-year', type='number', inputMode='numeric', value=1999,
+                  max=2010, min=1987, step=1, required=True),
+        html.Div(id='csv-output-info'),
+    ], style={'text-align': 'center'}),
 ])
+
+@callback(
+    Output('csv-graph', 'figure'),
+    Input('csv-year', 'value')
+)
+
+def update_map(selected_year):
+    selected_df = df[df.year == selected_year]
+
+    fig = px.choropleth(selected_df,
+                        locations='country',
+                        color='suicides/100k pop',
+                        hover_name='country')
 
 if __name__ == '__main__':
     app.run(debug=True)

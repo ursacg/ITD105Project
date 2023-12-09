185e1ecf74560076446db131f2793a779f4fd839
ursacg <ursabiacg@gmail.com>
2023-12-09 15:24:50 +0800

    added choropleth map to program

--- BigDataProject.py
+++ BigDataProject.py
@@ -1,36 +1,50 @@
 from dash import Dash, dcc, html, Input, Output, callback
 import plotly.express as px
 import pandas as pd
+#import base64
+#import io
 
 app = Dash(__name__)
 df = pd.read_csv('suicide-rate.csv')
+year_list = df['year'].unique()
 
 app.layout = html.Div([
+
     html.Div([
-        html.H1("Suicide Rates by Year", style={'textAlign': 'center'})
+        html.H1("Suicide Rates of Countries by Year",
+                style={'textAlign': 'center'})
     ]),
+
     html.Div([
-        dcc.Graph(id='csv-graph'),
+        dcc.Graph(id='csv-map'),
     ]),
+
     html.Div([
-        dcc.Input(id='csv-year', type='number', inputMode='numeric', value=1999,
-                  max=2010, min=1987, step=1, required=True),
-        html.Div(id='csv-output-info'),
-    ], style={'text-align': 'center'}),
+        html.P("Selected Year: "),
+        dcc.Dropdown(id='csv-year',
+                     options=[{'label': y, 'value': y} for y in year_list],
+                     placeholder='Select a year',
+                     searchable=False,
+                     clearable=False,
+                     style={'width': '40%'}),
+    ]),
+
 ])
 
 @callback(
-    Output('csv-graph', 'figure'),
+    Output('csv-map', 'figure'),
     Input('csv-year', 'value')
 )
-
 def update_map(selected_year):
     selected_df = df[df.year == selected_year]
 
     fig = px.choropleth(selected_df,
-                        locations='country',
+                        locations='country_iso',
                         color='suicides/100k pop',
-                        hover_name='country')
+                        hover_name='country',
+                        color_continuous_scale=px.colors.sequential.Plasma)
+    
+    return fig
 
 if __name__ == '__main__':
     app.run(debug=True)

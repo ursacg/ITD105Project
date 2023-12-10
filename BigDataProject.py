75b88af528a339495b8892d38eebf0e6402e9417
ursacg <ursabiacg@gmail.com>
2023-12-10 15:01:10 +0800

    added dropdowns for other categories

--- BigDataProject.py
+++ BigDataProject.py
@@ -6,7 +6,8 @@ import pandas as pd
 
 app = Dash(__name__)
 df = pd.read_csv('suicide-rate.csv')
-year_list = df['year'].unique()
+#year_list = df['year'].unique()
+country_list = df['country'].unique()
 
 app.layout = html.Div([
 
@@ -20,14 +21,83 @@ app.layout = html.Div([
     ]),
 
     html.Div([
-        html.P("Selected Year: "),
-        dcc.Dropdown(id='csv-year',
-                     options=[{'label': y, 'value': y} for y in year_list],
-                     value=2000,
-                     searchable=False,
-                     clearable=False,
-                     style={'width': '40%'}),
+        html.Label([
+                    "Selected Year: ",
+                    dcc.Dropdown(id='csv-year',
+                    #options=[{'label': y, 'value': y} for y in year_list],
+                    options=list(range(1986, 2016)),
+                    value=2000,
+                    searchable=False,
+                    clearable=False,
+                    style={'width': '40%'}),
+                    ]),
     ]),
+
+    html.Div([
+        html.Label([
+            "Selected Country: ",
+            dcc.Dropdown(
+                id='csv-country',
+                options=[{'label': c, 'value': c} for c in country_list],
+                value='Philippines',
+                multi=True,
+                clearable=False,
+                style={'width': '40%'}),
+                ]),
+        html.Br(),
+
+        html.Label([
+            "Selected Age: ",
+            dcc.Dropdown(
+                id='csv-age',
+                options=[
+                    {'label': '5-14 years', 'value': '5-14 years'},
+                    {'label': '15-24 years', 'value': '15-24 years'},
+                    {'label': '25-34 years', 'value': '25-34 years'},
+                    {'label': '35-54 years', 'value': '35-54 years'},
+                    {'label': '55-74 years', 'value': '55-74 years'},
+                    {'label': '75+ years', 'value': '75+ years'},
+                ],
+                value='5-14 years',
+                searchable=False,
+                clearable=False,
+                style={'width': '40%'}),
+                ]),
+        html.Br(),
+
+        html.Label([
+            "Selected Sex: ",
+            dcc.Dropdown(
+                id='csv-sex',
+                options=[
+                    {'label': 'Male', 'value': 'male'},
+                    {'label': 'Female', 'value': 'female'}
+                ],
+                value='male',
+                searchable=False,
+                clearable=False,
+                style={'width': '40%'}),
+                ]),
+        html.Br(),
+
+        html.Label([
+            "Selected Generation: ",
+            dcc.Dropdown(
+                id='csv-generation',
+                options=[
+                    {'label': 'G.I. Generation', 'value': 'G.I. Generation'},
+                    {'label': 'Silent Generation', 'value': 'Silent'},
+                    {'label': 'Baby Boomers Generation', 'value': 'Boomers'},
+                    {'label': 'Generation X', 'value': 'Generation X'},
+                    {'label': 'Millenials Generation', 'value': 'Millenials'},
+                    {'label': 'Generation Z', 'value': 'Generation Z'},
+                ],
+                value='Generation Z',
+                searchable=False,
+                clearable=False,
+                style={'width': '40%'}),
+                ])
+    ])
 
 ])
 
@@ -39,11 +109,16 @@ def update_map(selected_year):
     selected_df = df[df.year == selected_year]
 
     fig = px.choropleth(selected_df,
+                        locationmode='ISO-3',
                         locations='country_iso',
                         color='suicides/100k pop',
                         hover_name='country',
-                        color_continuous_scale=px.colors.sequential.Plasma)
-    
+                        hover_data=['country_iso', 'suicides/100k pop'],
+                        labels={'country_iso': 'Country Code (ISO-3)', 'suicides/100k pop': 'Suicide Rate (per 100k people)'},
+                        color_continuous_scale=px.colors.sequential.Plasma,
+                        range_color=(0, 225),
+                        width=1500,
+                        height=550)
     return fig
 
 if __name__ == '__main__':

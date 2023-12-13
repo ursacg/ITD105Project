92cd5cf1a501367f23fbf2e7fddbfe200fb10b2d
ursacg <ursabiacg@gmail.com>
2023-12-13 21:06:54 +0800

    added graph for predictive analysis

--- BigDataProject.py
+++ BigDataProject.py
@@ -1,21 +1,57 @@
+# Import packages for app
 from dash import Dash, dcc, html, Input, Output, callback
+from dash.exceptions import PreventUpdate
 import plotly.express as px
 import pandas as pd
-#import base64
-#import io
+#from simpledbf import Dbf5
 
+# Initialize Dash app
 app = Dash(__name__)
+
+# Load initial data
 df = pd.read_csv('suicide-rate.csv')
+
+# Create country list for dropdown
 country_list = [{'label': c, 'value': c} for c in df['country'].unique()]
 
+# App layout
 app.layout = html.Div([
 
+    # Page title
     html.Div([
         html.H1("Suicide Rate of Countries Based on Age and Sex (1986-2015)",
                 style={'textAlign': 'center'})
     ]),
 
+    # File upload
     html.Div([
+        html.Hr(),
+        html.H3("Upload CSV or DBF file"),
+        dcc.Upload(
+            id='upload-data',
+            children=html.Div([
+                'Drag and Drop or ',
+                html.A('Select File')
+            ]),
+            multiple=False,
+            style={
+                'width': '50%',
+                'height': '60px',
+                'lineHeight': '60px',
+                'borderWidth': '1px',
+                'borderStyle': 'solid',
+                'borderRadius': '5px',
+                'textAlign': 'center',
+                'margin': '10px'
+            },
+        ),
+        html.Div(id='output-upload-data'),
+    ]),
+
+    # Data selection
+    html.Div([
+
+        # Year selection
         html.Label([
                     "Selected Year: ",
                     dcc.Dropdown(id='csv-year',
@@ -27,6 +63,7 @@ app.layout = html.Div([
                     ),
                     ]),
 
+        # Age selection
         html.Label([
                     "Selected Age: ",
                     dcc.Dropdown(id='csv-age',
@@ -45,6 +82,7 @@ app.layout = html.Div([
                     ),
                     ]),
 
+        # Sex selection
         html.Label([
             "Selected Sex: ",
             dcc.Dropdown(
@@ -62,17 +100,30 @@ app.layout = html.Div([
 
             ]),
 
+    # Choropleth map output
     html.Div([
         dcc.Graph(id='csv-map'),
     ]),
 
+    html.Div([
+        html.Hr(),
+        html.H2("No. of Suicides, Population, Yearly GDP, and Yearly GDP per Capita of a Country",
+                style={'textAlign': 'center'})
+    ]),
+
+    # Country selection
     html.Div([
         html.Label([
             "Selected Country: ",
             dcc.Dropdown(
                 id='csv-country',
                 options=country_list,
-                value=['Philippines', 'South Africa', 'Brazil', 'United States', 'France', 'Australia'],
+                value=['Philippines',
+                       'South Africa',
+                       'Brazil',
+                       'United States',
+                       'France',
+                       'Australia'],
                 multi=True,
                 clearable=False,
                 style={'width': '50%'}
@@ -81,21 +132,38 @@ app.layout = html.Div([
 
     ]),
 
+    # No. of suicides line graph output
     html.Div([
         dcc.Graph(id='csv-graph-suicides')
     ]),
 
+    # Population line graph output
     html.Div([
         dcc.Graph(id='csv-graph-population')
     ]),
 
+    # GDP per year line graph output
     html.Div([
         dcc.Graph(id='csv-graph-gdp-year')
     ]),
 
+    # GDP per capita line graph output
     html.Div([
         dcc.Graph(id='csv-graph-gdp-capita')
-    ])
+    ]),
+
+    html.Div([
+        html.Hr()
+    ]),
+
+    html.Div([
+        html.H2("Predicted Rates ",
+                style={'textAlign': 'center'})
+    ]),
+
+    html.Div([
+        dcc.Graph(id='csv-graph-predict-suicide-rate')
+    ]),
 
 ])
 
@@ -143,7 +211,7 @@ def update_graph_suicide(selected_country, selected_age, selected_sex):
                       'suicides_no': 'No. of Suicides',
                       'country': 'Selected Country'
                   },
-                  title='No. of Suicides in a Country Based on Age and Sex',
+                  title='No. of Suicides Based on Age and Sex',
                   markers=True)
     return fig
 
@@ -167,7 +235,7 @@ def update_graph_population(selected_country, selected_age, selected_sex):
                       'population': 'Population',
                       'country': 'Selected Country'
                   },
-                  title='Population of a Country Based on Age and Sex',
+                  title='Population Based on Age and Sex',
                   markers=True)
     return fig
 
@@ -191,7 +259,7 @@ def update_graph_gdp_year(selected_country, selected_age, selected_sex):
                       'gdp_for_year($)': 'GDP per Year ($)',
                       'country': 'Selected Country'
                   },
-                  title='Yearly Gross Domestic Product (GDP) of a Country Based on Age and Sex',
+                  title='Yearly Gross Domestic Product (GDP) Based on Age and Sex',
                   markers=True)
     return fig
 
@@ -215,8 +283,32 @@ def update_graph_gdp_capita(selected_country, selected_age, selected_sex):
                       'gdp_per_capita($)': 'GDP per Capita ($)',
                       'country': 'Selected Country'
                   },
-                  title='Yearly Gross Domestic Product (GDP) per Capita of a Country Based on Age and Sex',
+                  title='Yearly Gross Domestic Product (GDP) per Capita Based on Age and Sex',
                   markers=True)
+    return fig
+
+@callback(
+    Output('csv-graph-predict-suicide-rate', 'figure'),
+    [Input('csv-country', 'value')],
+    Input('csv-age', 'value'),
+    Input('csv-sex', 'value')
+)
+def predict_graph_suicide_rate(selected_country, selected_age, selected_sex):
+    selected_df = df.loc[(df['country'].isin(selected_country)) &
+                         (df.age == selected_age) &
+                         (df.sex == selected_sex)]
+    
+    fig = px.scatter(selected_df,
+                     x='year',
+                     y='suicides_no',
+                     color='country',
+                     labels={
+                         'year': 'Year',
+                         'suicides_no': 'No. of Suicides',
+                         'country': 'Selected Country'
+                     },
+                     title='Predicted Projection oF Suicides Based on Age and Sex',
+                     trendline='ols',)
     return fig
 
 if __name__ == '__main__':
